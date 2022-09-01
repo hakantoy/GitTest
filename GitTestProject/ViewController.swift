a6a190b1d5c9d8b5bf6fce3df02878200e9273c2
Hakan TOY <toyhakan@outlook.com>
2022-09-01 08:39:35 +0300

    Initial Commit

---
+++ GitTestProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitTestProject
+//
+//  Created by Hakan TOY on 1.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
